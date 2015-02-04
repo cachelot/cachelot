8c43fbc28a517900df4db2b2e9b03249e42e1e40
aka.rider <aka.rider@gmail.com>
2015-02-04 02:51:49 +0200

    -- random utility functions moved from unit_test to common code, so they can be used in benchmarks / stress_tests -- cache_api -> cache_async_api -- small fixes in CMakeLists, compilation errors, etc

---
+++ src/cachelot/random.h
@@ -0,0 +1,53 @@
+#ifndef CACHELOT_RANDOM_H_INCLUDED
+#define CACHELOT_RANDOM_H_INCLUDED
+
+#include <random>
+
+/// @ingroup common
+/// @{
+
+namespace cachelot {
+
+    /// generate random integer in [min, max] range
+    template <typename IntType = int>
+    class random_int {
+    public:
+        explicit random_int(IntType minval, IntType maxval)
+            : m_rnd_gen(minval, maxval) {
+        }
+
+        random_int(const random_int &) = default;
+        random_int & operator= (const random_int &) = default;
+
+        IntType operator() () {
+            return m_rnd_gen(random_engine);
+        }
+    private:
+        typedef std::minstd_rand random_engine_type;
+        static random_engine_type random_engine;
+        std::uniform_int_distribution<IntType> m_rnd_gen;
+    };
+
+    template <typename IntType>
+    typename random_int<IntType>::random_engine_type random_int<IntType>::random_engine;
+
+    /// generate random string of `length` chars from pre-defined alphabet
+    inline string random_string(size_t length) {
+        static const char charset[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+        random_int<size_t> rnd_gen(0, sizeof(charset) - 1);
+        auto randchar = [&rnd_gen]() -> char {
+            return charset[ rnd_gen() ];
+        };
+        string str(length, 0);
+        std::generate_n(str.begin(), length, randchar);
+        return str;
+    }
+
+} // namespace cachelot
+
+/// @}
+
+#endif // CACHELOT_RANDOM_H_INCLUDED
