b77e434724d49ae36738dcad9efee9f4c7366e63
aka.rider <aka-rider@users.noreply.github.com>
2015-02-12 20:27:19 +0400

    -- io_buffer and tests

---
+++ src/unit_test/test_io_buffer.cpp
@@ -0,0 +1,35 @@
+#include "unit_test.h"
+#include <cachelot/io_buffer.h>
+#include <cstdlib>
+
+namespace {
+
+using namespace cachelot;
+
+BOOST_AUTO_TEST_SUITE(test_io_buffer)
+
+BOOST_AUTO_TEST_CASE(test_io_buffer_basic) {
+    io_buffer buf(0, 16);
+    BOOST_CHECK_EQUAL(0, buf.capacity());
+    static const char pattern[] = "Test string";
+    char * ptr = buf.begin_write(sizeof(pattern));
+    std::sprintf(ptr, pattern);
+    buf.confirm(sizeof(pattern));
+    BOOST_CHECK_EQUAL(buf.unread(), sizeof(pattern));
+    bytes read = buf.try_read_until(bytes::from_literal("\0"));
+    BOOST_CHECK_EQUAL(read.length(), sizeof(pattern));
+    BOOST_CHECK(std::memcmp(read.begin(), pattern, sizeof(pattern) - 1) == 0);
+    BOOST_CHECK_THROW(buf.begin_write(17), std::length_error);
+    buf.discard();
+    BOOST_CHECK_EQUAL(buf.unread(), 0);
+    ptr = buf.begin_write(16);
+    std::memset(ptr, 'X', 16);
+    buf.confirm(16);
+    read = buf.try_read_until(bytes::from_literal("XXXXXXXXXXXXXXXX"));
+    BOOST_CHECK_EQUAL(read.length(), 16);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+} // anonymouse namespace
+
