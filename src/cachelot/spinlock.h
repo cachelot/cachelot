2c0ab26212125493d40ec0b382fed1cd0179f6ee
Iurii Krasnoshchok <aka.rider@gmail.com>
2015-03-08 19:13:07 +0200

    cache service removed spinlock compilation fixes

--- src/cachelot/spinlock.h
+++ src/cachelot/spinlock.h
@@ -1,6 +1,8 @@
 #ifndef CACHELOT_SPINLOCK_H_INCLUDED
 #define CACHELOT_SPINLOCK_H_INCLUDED
 
+#include <atomic>
+
 /// @ingroup common
 /// @{
 
@@ -13,21 +15,34 @@ namespace cachelot {
      * It may give better performance on small locks by reducing latency, but for a long time locks 
      * (like IO waits) it is just a waste of CPU cycles.
      */
-    class spinlock : protected atomic_lock {
+    class spinlock {
+        static constexpr bool NOT_LOCKED = false;
     public:
+        spinlock()
+            : m_lock_flag(ATOMIC_FLAG_INIT)
+        {}
+
         /// acquire lock, wait until succeed
-        void lock() throw() { 
-            bool acquired;
-            do { 
-                acquired = acquire(); 
-            } while (!acquired);
+        void lock() noexcept {
+            bool locked = try_lock();
+            while (not locked) {
+                pause_cpu();
+                locked = try_lock();
+            }
         }
 
-        /// try to acquire lock, immediate return result
-        bool try_lock() throw() { return acquire(); }
+        /// try to acquire lock, immediately return result
+        bool try_lock() noexcept {
+            return NOT_LOCKED == m_lock_flag.test_and_set(std::memory_order_acquire);
+        }
         
         /// release lock
-        void unlock() throw() { release(); }
+        void unlock() noexcept {
+            m_lock_flag.clear(std::memory_order_release);
+        }
+
+    private:
+        std::atomic_flag m_lock_flag;
     };
  
 
