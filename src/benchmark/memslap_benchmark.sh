624cf9fe6b15f80f2c0f25d427c92241d60f8512
aka.rider <aka.rider@gmail.com>
2015-03-12 12:13:48 +0200

    benchmark: single thread memcached, fixes

--- src/benchmark/memslap_benchmark.sh
+++ src/benchmark/memslap_benchmark.sh
@@ -9,6 +9,9 @@ NUM_ITEMS=100000
 NUM_WARMUP_ITERATIONS=2
 NUM_REPEATS=16
 
+#TODO: Re-order tests, in a way to minimize cachelot / memcached restarts
+#      (Huge time economy on warmups)
+
 function ssh_command() {
     ssh "$REMOTE_SSH" "$*"
     return $?
@@ -43,7 +46,7 @@ function warmup() {
     local servers="$*"
     echo "Warmup $servers ..."
     for run in $(seq 1 $NUM_WARMUP_ITERATIONS); do
-        memslap "$servers" "set" "$NUM_ITEMS" "4" > /dev/null 2>&1
+        memslap "$servers" "set" "$NUM_ITEMS" "16" > /dev/null 2>&1
     done
     echo "---"
 }
@@ -55,7 +58,7 @@ function run_test_for() {
     local execute_times=$NUM_ITEMS
     # memory consumption is too much
     [ $concurrency -gt 16 ] && [ $execute_times -gt 10000 ] && let "execute_times = 10000"
-    #warmup "$servers"
+    warmup "$servers"
     echo " * set $execute_times items, concurrency: $concurrency"
     for t in $(seq 1 $NUM_REPEATS); do
         memslap "$servers" "set" "$execute_times"  "$concurrency"
@@ -79,8 +82,8 @@ function execute_benchmark() {
     run_test_for "-s $SERVER_ADDR:11211" "$concurrency"
     stop_cachelot_and_memcached
     echo "+++ memcached cluster"
-    ssh_bg_command $MEMCACHED -p 11213
-    ssh_bg_command $MEMCACHED -p 11214
+    ssh_bg_command $MEMCACHED -p 11213 -t 1
+    ssh_bg_command $MEMCACHED -p 11214 -t 1
     run_test_for "-s $SERVER_ADDR:11213 -s $SERVER_ADDR:11214" "$concurrency"
     stop_cachelot_and_memcached
     echo "+++ cachelot cluster"
@@ -88,12 +91,16 @@ function execute_benchmark() {
     ssh_bg_command $CACHELOT -p 11216
     run_test_for "-s $SERVER_ADDR:11215 -s $SERVER_ADDR:11216" "$concurrency"
     stop_cachelot_and_memcached
+    echo "+++ memcached single thread"
+    ssh_bg_command $MEMCACHED -p 11217 -t 1
+    run_test_for "-s $SERVER_ADDR:11217" "$concurrency"
+    stop_cachelot_and_memcached
 }
 
 trap "stop_cachelot_and_memcached; exit" SIGHUP SIGINT SIGTERM
 
-# concurrency: 1, 2, 4, 6, ... 
+# concurrency: 1, 2, 4, 6, ...
 for concurrency in $(echo "1" && seq 2 2 32); do
     execute_benchmark $concurrency
 done
-    
+
