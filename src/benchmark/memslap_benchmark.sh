4b8f0fe4c642c73b3174c96aa1600baab1d60ff2
Iurii Krasnoshchok <aka.rider@gmail.com>
2015-03-14 02:09:21 +0200

    memslap benchmark tests reorder for the time economy

--- src/benchmark/memslap_benchmark.sh
+++ src/benchmark/memslap_benchmark.sh
@@ -9,20 +9,20 @@ NUM_ITEMS=100000
 NUM_WARMUP_ITERATIONS=2
 NUM_REPEATS=16
 
-#TODO: Re-order tests, in a way to minimize cachelot / memcached restarts
-#      (Huge time economy on warmups)
 
 function ssh_command() {
     ssh "$REMOTE_SSH" "$*"
     return $?
 }
 
+
 function ssh_bg_command() {
     local remote_command=$(echo "nohup $* > /dev/null 2>&1 < /dev/null & ; sleep .3 ; ps -p \$! > /dev/null || exit 1")
     echo "running \"$remote_command\" ..."
     ssh "$REMOTE_SSH" "$remote_command" || exit 1
-    sleep 2
+    sleep 10
 }
+
 
 function memslap() {
     local servers="$1"
@@ -42,6 +42,7 @@ function stop_cachelot_and_memcached() {
     sleep 2
 }
 
+
 function warmup() {
     local servers="$*"
     echo "Warmup $servers ..."
@@ -59,48 +60,62 @@ function run_test_for() {
     # memory consumption is too much
     [ $concurrency -gt 16 ] && [ $execute_times -gt 10000 ] && let "execute_times = 10000"
     warmup "$servers"
-    echo " * set $execute_times items, concurrency: $concurrency"
-    for t in $(seq 1 $NUM_REPEATS); do
-        memslap "$servers" "set" "$execute_times"  "$concurrency"
-    done
-    echo " * get $execute_times items, concurrency: $concurrency"
-    for t in $(seq 1 $NUM_REPEATS); do
-        memslap "$servers" "get" "$execute_times"  "$concurrency"
+    for concurrency in $(echo "1" && seq 2 2 32); do
+        echo " *** BEGIN: set $execute_times items, concurrency: $concurrency"
+        for t in $(seq 1 $NUM_REPEATS); do
+            memslap "$servers" "set" "$execute_times"  "$concurrency"
+        done
+        echo " *** END: set $execute_times items, concurrency: $concurrency"
+        echo ""
+        echo " *** BEGIN: get $execute_times items, concurrency: $concurrency"
+        for t in $(seq 1 $NUM_REPEATS); do
+            memslap "$servers" "get" "$execute_times"  "$concurrency"
+        done
+        echo " *** END: get $execute_times items, concurrency: $concurrency"
+        echo ""
     done
 }
 
-function execute_benchmark() {
-    local concurrency=$1
-    echo "*** CONCURRENCY: $concurrency ******************************"
-    stop_cachelot_and_memcached
-    echo "+++ cachelot"
-    ssh_bg_command $CACHELOT -p 11212
-    run_test_for "-s $SERVER_ADDR:11212" "$concurrency"
-    stop_cachelot_and_memcached
-    echo "+++ memcached"
-    ssh_bg_command $MEMCACHED -p 11211
-    run_test_for "-s $SERVER_ADDR:11211" "$concurrency"
-    stop_cachelot_and_memcached
-    echo "+++ memcached cluster"
-    ssh_bg_command $MEMCACHED -p 11213 -t 1
-    ssh_bg_command $MEMCACHED -p 11214 -t 1
-    run_test_for "-s $SERVER_ADDR:11213 -s $SERVER_ADDR:11214" "$concurrency"
-    stop_cachelot_and_memcached
-    echo "+++ cachelot cluster"
-    ssh_bg_command $CACHELOT -p 11215
-    ssh_bg_command $CACHELOT -p 11216
-    run_test_for "-s $SERVER_ADDR:11215 -s $SERVER_ADDR:11216" "$concurrency"
-    stop_cachelot_and_memcached
-    echo "+++ memcached single thread"
-    ssh_bg_command $MEMCACHED -p 11217 -t 1
-    run_test_for "-s $SERVER_ADDR:11217" "$concurrency"
-    stop_cachelot_and_memcached
+
+function print_title() {
+    local title=$1
+    echo "********************************************************"
+    echo "+++ $title"
+    echo "********************************************************"
 }
 
+## Main ----------------------------------------------------------
 trap "stop_cachelot_and_memcached; exit" SIGHUP SIGINT SIGTERM
+stop_cachelot_and_memcached
 
-# concurrency: 1, 2, 4, 6, ...
-for concurrency in $(echo "1" && seq 2 2 32); do
-    execute_benchmark $concurrency
-done
+print_title("cachelot")
+ssh_bg_command $CACHELOT -p 11212
+run_test_for "-s $SERVER_ADDR:11212" "$concurrency"
+stop_cachelot_and_memcached
+sleep 60
+
+print_title "memcached"
+ssh_bg_command $MEMCACHED -p 11211
+run_test_for "-s $SERVER_ADDR:11211" "$concurrency"
+stop_cachelot_and_memcached
+sleep 60
+
+print_title "memcached cluster"
+ssh_bg_command $MEMCACHED -p 11213 -t 1
+ssh_bg_command $MEMCACHED -p 11214 -t 1
+run_test_for "-s $SERVER_ADDR:11213 -s $SERVER_ADDR:11214" "$concurrency"
+stop_cachelot_and_memcached
+sleep 60
+
+print_title "memcached single thread"
+ssh_bg_command $MEMCACHED -p 11217 -t 1
+run_test_for "-s $SERVER_ADDR:11217" "$concurrency"
+stop_cachelot_and_memcached
+sleep 60
+
+print_title "cachelot cluster"
+ssh_bg_command $CACHELOT -p 11215
+ssh_bg_command $CACHELOT -p 11216
+run_test_for "-s $SERVER_ADDR:11215 -s $SERVER_ADDR:11216" "$concurrency"
+stop_cachelot_and_memcached
 
