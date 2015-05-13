1f635eac11e7085b300f70c64775ecf646a737bb
Iurii Krasnoshchok <aka.rider@gmail.com>
2015-05-14 01:30:53 +0300

    Server test script refactoring and new tests

--- src/test/server_test.py
+++ src/test/server_test.py
@@ -4,9 +4,12 @@
 #
 
 import sys
+import logging
 import random
 import string
 import memcached
+
+log = logging.Logger('Cachelot Test')
 
 
 class TestFailedError(Exception):
@@ -18,23 +21,21 @@ def CHECK(condition):
         raise TestFailedError()
 
 
-def random_string(minlen, maxlen):
-    assert minlen > 1, 'minlen is too small'
-    assert minlen <= maxlen, 'invalid lengths range'
-    length = random.randint(minlen, maxlen)
-    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+KEY_ALPHABET = string.ascii_letters + string.digits
 
 
 def random_key():
-    return random_string(10, 50)
-
+    length = random.randint(10, 250)
+    return ''.join(random.choice(KEY_ALPHABET) for _ in range(length))
+    
 
 def random_value():
-    return random_string(5, 1000)
+    length = random.randint(1, 524288)
+    return ''.join(chr(random.randint(0, 254)) for _ in range(length))
 
 
-def smoke_test(mc):
-    "Test basic functionality"
+def basic_storage_test(mc):
+    "Test set/get/add/delete/replace commands"
     key1 = random_key()
     value1 = random_value()
     # replace, add, get, delete
@@ -46,30 +47,37 @@ def smoke_test(mc):
     CHECK( mc.replace(key1, value1) == True )
     CHECK( mc.get(key1) == value1 )
     CHECK( mc.delete(key1) == True )
-    # set, cas
-    key2 = random_key()
-    value2 = random_value()
-    mc.set(key2, value2)
-    CHECK( mc.get(key2) == value2 )
-    k2_ret_value, k2_cas = mc.gets(key2)
-    CHECK( k2_ret_value == value2 )
-    value2 = random_value()
-    CHECK( mc.cas(key2, value2, 0, k2_cas) == True )
-    CHECK( mc.cas(key2, value2, 0, k2_cas) == False )  # second cas shall fail
-    CHECK( mc.get(key2) == value2 )
-    # cas with replace
-    k2_ret_value, k2_cas = mc.gets(key2)
-    value2 = random_value()
-    mc.replace(key2, value2)
-    CHECK( mc.cas(key2, value2, 0, k2_cas) == False )  # cas after replace shall fail
-    CHECK( mc.get(key2) == value2 )
-    # cas with set
-    k2_ret_value, k2_cas = mc.gets(key2)
-    value2 = random_value()
-    mc.set(key2, value2)
-    CHECK( mc.cas(key2, value2, 0, k2_cas) == False )  # cas after set shall fail
-    CHECK( mc.get(key2) == value2 )
-    # multikey get, gets
+   
+
+def basic_cas_test(mc):
+    "Test cas (compare and swap) command"
+    # simple cas
+    k = random_key()
+    v = random_value()
+    mc.set(k, v)
+    CHECK( mc.get(k) == v )
+    retval, ver = mc.gets(k)
+    CHECK( retval == v )
+    v = random_value()
+    CHECK( mc.cas(k, v, 0, ver) == True )
+    CHECK( mc.cas(k, v, 0, ver) == False )  # second cas shall fail
+    CHECK( mc.get(k) == v )
+    # cas after replace
+    __, ver = mc.gets(k)
+    v = random_value()
+    mc.replace(k, v)
+    CHECK( mc.cas(k, v, 0, ver) == False )  # cas after replace shall fail
+    CHECK( mc.get(k) == v )
+    # cas after set
+    __, ver = mc.gets(k)
+    v = random_value()
+    mc.set(k, v)
+    CHECK( mc.cas(k, v, 0, ver) == False )  # cas after set shall fail
+    CHECK( mc.get(k) == v )
+
+
+def basic_batch_op_test(mc):
+    "Test multi-get and batch set operations"
     test_dict = {}
     for i in range(100):
         k = random_key()
@@ -80,18 +88,37 @@ def smoke_test(mc):
         CHECK( test_dict[k] == v )
 
 
-def fuzzy_test(mc):
+def basic_expiration_test(mc):
+    "Test object expiration and touch command"
+    pass
+
+
+def basic_arithmetic_test(mc):
+    "Test incr/decr commands"
+    pass
+
+
+def run_smoke_test(mc):
+    "Test basic functionality"
+    for run_no in range(100):
+        basic_storage_test(mc)
+        basic_cas_test(mc)
+        basic_batch_op_test(mc)
+        basic_expiration_test(mc)
+        basic_arithmetic_test(mc)
+
+
+def run_fuzzy_test(mc):
     for num_items in range(10000):
         pass
 
 
 def main():
     mc = memcached.connect_tcp('localhost', 11211)
-    smoke_test(mc)
+    run_smoke_test(mc)
 
 
 if __name__ == '__main__':
-    import logging
     logging.basicConfig(level=logging.DEBUG)
     main()
 
