b55c312618c2e76031b409815786112c4f864011
Iurii Krasnoshchok <aka.rider@gmail.com>
2015-05-23 14:51:33 +0300

    io_buffer discard_written test

--- src/unit_test/test_io_buffer.cpp
+++ src/unit_test/test_io_buffer.cpp
@@ -12,19 +12,28 @@ BOOST_AUTO_TEST_CASE(test_io_buffer_basic) {
     io_buffer buf(0, 16);
     BOOST_CHECK_EQUAL(0, buf.capacity());
     static const char pattern[] = "Test string";
+    // write test pattern
     char * ptr = buf.begin_write(sizeof(pattern));
     std::sprintf(ptr, pattern);
     buf.complete_write(sizeof(pattern));
     BOOST_CHECK_EQUAL(buf.non_read(), sizeof(pattern));
+    // read from buffer
     bytes read = buf.try_read_until(bytes::from_literal("\0"));
     BOOST_CHECK_EQUAL(read.length(), sizeof(pattern));
     BOOST_CHECK(std::memcmp(read.begin(), pattern, sizeof(pattern) - 1) == 0);
     BOOST_CHECK_THROW(buf.begin_write(17), std::length_error);
+    // reset buffer
     buf.discard_all();
     BOOST_CHECK_EQUAL(buf.non_read(), 0);
+    // write 16 bytes of nothing and discard it immediately
+    ptr = buf.begin_write(16);
+    buf.complete_write(16);
+    buf.discard_written();
+    // write 16 bytes of 'X'
     ptr = buf.begin_write(16);
     std::memset(ptr, 'X', 16);
     buf.complete_write(16);
+    // read and check
     read = buf.try_read_until(bytes::from_literal("XXXXXXXXXXXXXXXX"));
     BOOST_CHECK_EQUAL(read.length(), 16);
 }
