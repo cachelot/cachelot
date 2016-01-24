ac91405ce50190ee6e6cd0a2795b6a01bef5e625
Iurii Krasnoshchok <aka.rider@gmail.com>
2016-01-24 22:15:51 +0100

    build: fixed dest dir; do not build memalloc_benchmark with ASAN

--- all_build.sh
+++ all_build.sh
@@ -8,9 +8,6 @@ for cfg in ${BUILD_CFGS[@]}; do
   clean_build_files
   echo "Building ${cfg} ..."
   cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=${cfg} &&
-  make -j 4 &&
-  mkdir -p "./bin/${cfg}" &&
-  find './bin' -maxdepth 1 -type f -exec mv {} "./bin/${cfg}/" \; &&
-  echo "All files in ./bin/${cfg}" || break
+  make -j 2 || exit 1
 done
 
