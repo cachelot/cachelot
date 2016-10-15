a64d1891253a44e66549e7f1f28413eb9f9df146
Iurii Krasnoshchok <aka.rider@gmail.com>
2016-10-15 17:56:53 +0200

    build: fixed cleanup script removed pkg/docker/alpine/Makefile

--- cleanup.sh
+++ cleanup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+ROOT=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
 function clean_artifacts {
-    rm -rf ./bin/*
-    rm -rf ./lib/*
-    rm -rf doc/Doxygen/*
+    rm -rf "${ROOT}/bin/*"
+    rm -rf "${ROOT}/lib/*"
+    rm -rf "${ROOT}/doc/Doxygen/*"
 }
 
 function clean_build_files {
-    find . -type d -and \( -name "CMakeFiles" -or -name "CMakeScripts" -or -name "*.build" -or -name "*.xcodeproj" \) | xargs rm -r > /dev/null 2>&1
-    find . -type f -and \( -name "CMakeCache.txt" -or -name "cmake_install.cmake" -or -name "Makefile" -or -name "*.pyc" \) | xargs rm > /dev/null 2>&1
+    find "${ROOT}" -type d -and \( -name "CMakeFiles" -or -name "CMakeScripts" -or -name "*.build" -or -name "*.xcodeproj" \) | xargs rm -r > /dev/null 2>&1
+    find "${ROOT}" -type f -and \( -name "CMakeCache.txt" -or -name "cmake_install.cmake" -or -name "*.pyc" -or -name "*.xctestrun" \) | xargs rm > /dev/null 2>&1
+    rm -f Makefile
+    rm -f src/*/Makefile
 }
 
 clean_artifacts
